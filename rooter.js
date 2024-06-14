fb69b8e1bbe9afff8c67708d5d45a1234a98d26a
cannlz <52014546+cannlz@users.noreply.github.com>
2024-06-14 21:18:27 +0400

    add switch case

--- rooter.js
+++ rooter.js
@@ -7,26 +7,32 @@ function rooter(req, resp, users, path, method) {
     // Заголовки ответа
     resp.setHeader('Content-Type', 'application/json');
 
-    if (path === '/users' && method === 'GET') {
-        // Получить список пользователей/одного пользователя, если передать его id
-        getUser(req, resp, users);
+    switch(path, method) {
+        case '/users', 'GET':
+            // Получить список пользователей/одного пользователя, если передать его id
+            getUser(req, resp, users);
+            break;
+        
+        case '/users', 'POST':
+            // Создать пользователя по его ID
+            createUser(req, resp, users);
+            break;
 
-    } else if (path === '/users' && method === 'POST') {
-        // Создать пользователя по его ID
-        createUser(req, resp, users);
+        case '/users', 'PUT':
+            // Обновить пользователя по ID
+            updateUser(req, resp, users);
+            break;
+        
+        case '/users', 'DELETE':
+            // Удалить пользователя по его ID
+            deleteUser(req, resp, users);
+            break;
 
-    } else if (path === '/users' && method === 'PUT') {
-        // Обновить пользователя по ID
-        updateUser(req, resp, users);
-
-    } else if (path === '/users' && method === 'DELETE') {
-        // Удалить пользователя по его ID
-        deleteUser(req, resp, users);
-
-    } else {
-        // Если что-то пошло не так(не тот path или method), выдаём 404 с ошибкой
-        resp.writeHead(404);
-        resp.end(JSON.stringify({message: "Route nor found"}));
+        default:
+            // Если что-то пошло не так(не тот path или method), выдаём 404 с ошибкой
+            resp.writeHead(404);
+            resp.end(JSON.stringify({message: "Route nor found"}));
+            break;
     }
 }
 
